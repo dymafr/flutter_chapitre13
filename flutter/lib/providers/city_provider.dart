9da3f66d5368c3d24762579c88a7fb69b8967305
dymafr <contact@dyma.fr>
2023-03-15 14:18:43 +0100

    initial commit

---
+++ flutter/lib/providers/city_provider.dart
@@ -0,0 +1,33 @@
+import 'dart:collection';
+import 'dart:convert';
+import 'package:flutter/cupertino.dart';
+import '../models/city_model.dart';
+import 'package:http/http.dart' as http;
+
+class CityProvider extends ChangeNotifier {
+  final String host = 'localhost';
+  List<City> _cities = [];
+  bool isLoading = false;
+
+  UnmodifiableListView<City> get cities => UnmodifiableListView(_cities);
+
+  City getCityByName(String cityName) =>
+      cities.firstWhere((city) => city.name == cityName);
+
+  Future<void> fetchData() async {
+    try {
+      isLoading = true;
+      http.Response response = await http.get(Uri.http(host, '/api/cities'));
+      if (response.statusCode == 200) {
+        _cities = (json.decode(response.body) as List)
+            .map((cityJson) => City.fromJson(cityJson))
+            .toList();
+        isLoading = false;
+        notifyListeners();
+      }
+    } catch (e) {
+      isLoading = false;
+      rethrow;
+    }
+  }
+}
---
+++ flutter/lib/views/home/home_view.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import '../../providers/city_provider.dart';
+import '../../models/city_model.dart';
+import '../../widgets/dyma_drawer.dart';
+import '../../widgets/dyma_loader.dart';
+import 'widgets/city_card.dart';
+
+class HomeView extends StatefulWidget {
+  static const String routeName = '/';
+
+  const HomeView({super.key});
+
+  @override
+  State<HomeView> createState() {
+    return _HomeState();
+  }
+}
+
+class _HomeState extends State<HomeView> {
+  @override
+  Widget build(BuildContext context) {
+    List<City> cities = Provider.of<CityProvider>(context).cities;
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('dymatrip'),
+      ),
+      drawer: const DymaDrawer(),
+      body: Container(
+        padding: const EdgeInsets.all(10),
+        child: RefreshIndicator(
+          onRefresh: Provider.of<CityProvider>(context, listen: false).fetchData,
+          child: cities.isNotEmpty
+              ? ListView.builder(
+            itemCount: cities.length,
+            itemBuilder: (_, i) => CityCard(
+              city: cities[i],
+            ),
+          )
+              : const DymaLoader(),
+        ),
+      ),
+    );
+  }
+}
